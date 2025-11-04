b7f26fc6f47378e1b049b94f7e2f0c4bd33ba5e0
Ankush Kumar Jha <ankushjha-24@users.noreply.github.com>
2025-11-04 13:52:15 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path:'./env'
 });
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+}).catch((error) => {
+    console.error("Failed to start the server:", error);
+});
---
+++ src/utils/apiError.js
@@ -0,0 +1,3 @@
+class apiError extends Error{
+    
+}
