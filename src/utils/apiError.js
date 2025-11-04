9df309bdba7d3b53656de65825dad3cbdff758a6
Ankush Kumar Jha <ankushjha-24@users.noreply.github.com>
2025-11-04 15:06:57 +0530

    added utility

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,3 +1,16 @@
 class apiError extends Error{
+    constructor(statuscode,statck="",error=[],message="Error"){
+        super(message);
+        this.statuscode=statuscode;
+        this.message=message;
+        this.error=error;
+        this.success=false;
+        if(statck){
+            this.stack=statck;
+        }else{
+            Error.captureStackTrace(this,this.constructor);
+        }
+    }
     
 }
+export {apiError};
